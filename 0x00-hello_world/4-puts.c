90af2643798dc78c1e963394f33015213259ca51
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-16 12:38:19 -0700

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
-
+/**
+ * main - Entry Point
+ * 
+ * Return: Always 0 (success)
+ */
 int main(void)
 
 {
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
