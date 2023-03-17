1a6ba7ec02eee3198b27fd98d6e52dc23319e2eb
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-17 13:18:42 -0700

    more alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return alphabets
+ */
+int main(void)
+{
+	char a = 'a';
+	char b = 'A';
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	for (b = 'A'; b <= 'Z'; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
