06efe31173dc8f183f4a771e384f564708589c64
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-21 16:37:25 -0700

    absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - computes absolute values
+ * @n: parameter
+ * Return: returns absolute value
+ */
+int _abs(int n)
+{
+	abs(n);
+	return (-n);
+}
