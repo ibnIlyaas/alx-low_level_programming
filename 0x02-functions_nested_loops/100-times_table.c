75488797b9b05021b5092acb5c336824ab768324
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-21 18:50:18 -0700

    fib

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * print_time_table - prints n timestable
+ * @n: integer argurment
+ */
+void print_times_table(int n)
+
+
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints first 100 fibonancci
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	long int a, b, c, d;
+	b = 1;
+	c = 2;
+	for (a = 1; a <= 50; ++a)
+	{
+		if (b != 20365011074)
+		{
+			printf("%ld, ", b);
+		}
+		else
+		{
+			printf("%ld\n", b);
+		}
+		d = b + c;
+		b = c;
+		c = d;
+	}
+	return (0);
+}
