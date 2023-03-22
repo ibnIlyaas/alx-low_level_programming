feafbc1ca27f2a45cd971be9a0ce6867bd4e3b89
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-21 19:01:09 -0700

    nats

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of mutliples of 5 and 3 below 1024
+ * Return: Alway 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int sum;
+
+	sum = 0;
+	for (n = 0; n < 1023; n++)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
