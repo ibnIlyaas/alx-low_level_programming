92de7072f2c3aafe1050d992d9728559f2395e0b
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-21 19:25:02 -0700

    timestable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,49 @@
 #include "main.h"
 /**
- * print_time_table - prints n timestable
+ * print_times_table - prints n timestable
  * @n: integer argurment
  */
 void print_times_table(int n)
+{
+	int r;
+	int c;
+	int p;
 
+	if (n >= 0 && n <= 15)
+	{
+		for (r = 0; r <= n; r++)
+		{
+			for (c = 0; c <= n; c++)
+			{
+				p = (r * c);
+				if (c == 0)
+					_putchar('0' + p);
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (p <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + p);
+					}
+					else if (p > 9 && p < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (p / 10));
+						_putchar('0' + (p % 10));
+					}
+					else if (p >= 100)
+					{
+						_putchar('0' + (p / 100));
+						_putchar('0' + ((p / 10) % 10));
+						_putchar('0' + (p % 10));
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
 
