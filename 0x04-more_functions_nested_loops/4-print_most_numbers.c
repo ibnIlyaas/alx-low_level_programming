1596dae647361dc3a403f07bd2592191c60e87c8
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-23 15:57:38 -0700

    more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	char c = '0';
+	char c;
 
-	while (c <= '9')
+	for (c = '0'; c <= '9'; c++)
 	{
 		if (c != '2' && c != '4')
 		{
 			_putchar(c);
-			c++;
 		}
 	}
 	_putchar('\n');
