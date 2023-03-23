b88a09aa3bad9850b356388c92a0947eacea519f
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-23 13:43:39 -0700

    main.h and isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - checks for uppercase
+ * @c: parameter
+ *
+ *Return: 0 or 1 depending on condition
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
