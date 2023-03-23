2a172a3493b69836a9fe428b10984c96c80724f6
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-22 21:02:49 -0700

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests positive_or_negative function
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This directory contains information on debugging and its processes
