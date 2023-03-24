5f300fc4b73324266bc9469e040ffd47392addaa
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-23 17:20:01 -0700

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,9 +10,13 @@ void print_line(int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		if (i > 0)
+		if (i < 0)
 		{
-		_putchar('_');
+		_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
 		}
 	}
 	_putchar('\n');
