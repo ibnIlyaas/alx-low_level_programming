9a1a08de9d5f23ef05e94e20395fa49bde90e0ea
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-23 22:32:20 -0700

    straigt line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
  * print_line - prints a line based on n
- *
- * @n: parameter input
+ * @n: input param
  */
 
 void print_line(int n)
 {
+
 	int i = n;
-	
+
 	for (i = n; i > 0; i--)
 		_putchar('_');
-	
+
 	_putchar('\n');
 }
