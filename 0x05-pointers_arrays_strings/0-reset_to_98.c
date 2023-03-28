a0aa8e5b838def40f4135d0745010374124d372d
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-27 17:10:43 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of pointer to 98
+ *
+ * @n: int param
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/READM.md
@@ -0,0 +1,2 @@
+Pointers are powerful instruments in C.
+This Directory will contain tests and projects on pointers and arrayys and strings
