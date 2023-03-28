268b58bbe19c8ae33dbdafbb6ef764b9b5f4f7ea
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-28 16:35:51 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string param
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int rv;
+
+	while (*s != '\n')
+	{
+		len++;
+		++s;
+	}
+	s--;
+
+	for (rv = len; rv > 0; rv--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
