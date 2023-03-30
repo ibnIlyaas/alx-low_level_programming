7c983186c1a814cbadb76be016322e62eab96c4d
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-30 15:01:19 -0700

    Capitalize each word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,21 +2,23 @@
 #include <ctype.h>
 
 /**
- * cap_string - capitalizes each word of a string
- * @s: string to be capitalized
- * Return: s
+ * cap_string - capitalizes each word in a string
+ * @s: string parameter
+ * Return: string
  */
 
 char *cap_string(char *s)
 {
 	int i = 0;
 
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 'a' + 'A';
+	i++;
+
 	while (s[i] != '\0')
 	{
-		if (s[i] >= 'a' && s[i] i<= 'z' && isspace(s[i - 1]))
-		{
-			s[i] = toupper(s[i]);
-		}
+		if (s[i] >= 'a' && s[i] <= 'z' && isspace(s[i - 1]))
+			s[i] = s[i] - 'a' + 'A';
 		i++;
 	}
 	return (s);
