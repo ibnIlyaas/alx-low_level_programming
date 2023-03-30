6062a94829f71ea08c8db0d2d0988dfa7c398d3a
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-30 15:20:56 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,6 +10,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	strcmp(*s1, *s2);
-	return (0);
+	int i = 0;
+
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+		i++;
+	if (*(s2 + i))
+		return (*(s1 + i) - *(s2 + i));
+	else
+		return (0);
 }
