808b4f24da973cfef3a265dabc290b4beadc3573
Ibn Ilyaas <baeliasu@gmail.com>
2023-03-30 08:06:03 -0700

    to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes lowercase letters to uppercase
+ * @str: string param
+ * Return: str
+ */
+
+char *string_toupper(char *str)
+{
+	int index;
+
+	while (str[index] != '\0')
+	{
+		if (*str[index] >= 'a' && *str[index] <= 'z')
+		{
+			*str[index] = *str[index] - 32;
+		}
+		index++;
+	}
+	return (str);
+}
+
