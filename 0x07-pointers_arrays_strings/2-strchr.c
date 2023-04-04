695b76a1960c25cc627e77430a1b60fb65033e5e
Ibn Ilyaas <baeliasu@gmail.com>
2023-04-03 17:22:55 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - searches for the first occurence of a character
+ * @s: string to be searched
+ * @c: character to be searched for
+ * Return: pointer to first occurence of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+	return (c);
+}
