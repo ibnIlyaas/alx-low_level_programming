d6403eac6c4aab91d9486081e3d4ebfa735a8c9b
Ibn Ilyaas <baeliasu@gmail.com>
2023-04-03 17:44:29 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - finds the length of a substring 
+ * @s: main string to be searched from
+ * @accept: string to be compared to
+ * Return: Lenght of substring
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int len; /* lenght of substring */
+
+	len = strspn(s, accept);
+	return (len);
+}
