5046ce21721c72e30856f9abce2cab66e8a72aaa
Ibn Ilyaas <baeliasu@gmail.com>
2023-04-05 18:20:40 -0700

    strlen rcs

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - returns lenght of a string
+ * @s: String param
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = strlen(s);
+	return (len);
+}
