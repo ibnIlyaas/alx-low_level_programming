97b65fc31ebbe2bab5513c2511133311322c3204
Ibn Ilyaas <baeliasu@gmail.com>
2023-04-09 16:17:39 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -21,8 +21,8 @@ int main(int argc, char *argv[])
 	}
 	cents = atoi(argv[1]);
 
-	if (cents < 0)
-		printf("0\n");
+	/*if (cents < 0)*/
+		/*printf("0\n");*/
 	while (cents > 0)
 	{
 		if (cents >= 25)
