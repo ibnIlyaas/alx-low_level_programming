fca267b651268ad88ae239fa1b586dc3cb454095
Ibn Ilyaas <baeliasu@gmail.com>
2023-04-09 06:09:39 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,15 +14,18 @@ int main(int argc, char *argv[])
 	int i = 0;
 	int product = 1;
 
-	(void)argc;
-	for (i = 1; i < argc; i++)
+	if (argc > 1)
+	
+		for (i = 1; i < argc; i++)
+		{
+			product *= atoi(argv[i]);
+			printf("%d\n", product);
+		}
+	else
 	{
-		if(i < 2)
-			printf("Error\n");
-		return (0);
-
-		product *= atoi(argv[i]);
+		printf("Error\n");
+		return (1);
 	}
-	printf("%d\n", product);
+
 	return (0);
 }
