a0a2cabd31d56e8eb68e468d79c86ecbd3ec5d67
Ibn Ilyaas <baeliasu@gmail.com>
2023-04-18 09:40:23 -0700

    dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,7 @@ typedef struct dog
 	float age;
 	char *owner;
 }
+dog_t;
 /*funx prototypes*/                                               void init_dog(struct dog *d, char *name, float age, char *owner); void print_dog(struct dog *d);                                    dog_t *new_dog(char *name, float age, char *owner);               void free_dog(dog_t *d);
 
 #endif /*_DOG_H_*/
