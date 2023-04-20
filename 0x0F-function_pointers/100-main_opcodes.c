b6b8e32428f390b1d78cc0065c1dc09f3485cdec
Ibn Ilyaas <baeliasu@gmail.com>
2023-04-20 09:05:04 -0700

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,19 +15,19 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
 	bytes = atoi(argv[1]);
 
 	if (bytes < 0)
 	{
 		printf("Error\n");
-		return(2);
+		return (2);
 	}
 	for (i = 0; i < bytes; i++)
 	{
 		printf("%02x", abc[i] & 0xFF);
-		if (i != bytes -1)
+		if (i != bytes - 1)
 			printf(" ");
 	}
 	printf("\n");
