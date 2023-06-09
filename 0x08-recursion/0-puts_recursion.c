2d86b5bacb37a5631384450f67acfebe106cb983
ibnIlyaas <baeliasu@gmail.com>
2023-06-08 18:13:00 -0700

    Print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') // check if end of string
+	if (*s == '\0') /* check if end of string */
 	{
 		_putchar('\n');
 		return;
 	}
 
 	_putchar(*s);
-	_puts_recursion(s + 1); //iterates through string recursively
+	_puts_recursion(s + 1); /* iterates through string recursively */
 }
