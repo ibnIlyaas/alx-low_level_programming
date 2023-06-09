c4866c41756fe07f21b9193bd8365095b9ce5b5c
ibnIlyaas <baeliasu@gmail.com>
2023-06-08 18:32:31 -0700

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _print_rev-recursion - reverses a string recursively
+ * @s: String param
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
