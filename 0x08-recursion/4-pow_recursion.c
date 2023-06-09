0ad8aac78861f117c1632bb2441f42474f3bb081
ibnIlyaas <baeliasu@gmail.com>
2023-06-08 20:05:13 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,8 +15,6 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	res = x * _pow_recursion(x, y-1);
-	return res;
+	res = x * _pow_recursion(x, y - 1);
+	return (res);
 }
-
-	
